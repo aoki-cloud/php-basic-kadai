e644134ea46c0548a424d8bbdaf8f382c982f122
aoki-cloud <takosukeaoki@icloud.com>
2024-08-03 13:01:26 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP KADAI_007</title>
+</head>
+<body>
+    <p>
+        <?php
+        //連想配列に値を代入する
+        $category_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($category_data);
+        ?>
+    </p>
+</body>
+
+</html>
